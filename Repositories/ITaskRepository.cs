c6a0612b8cf2a4de4682fe5782f8ab9fa25d0004
JLyp <jakublypik@gmail.com>
2022-01-27 02:38:22 +0100

    Add project files.

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using RentApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentApplication.Repositories
+{
+  public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
